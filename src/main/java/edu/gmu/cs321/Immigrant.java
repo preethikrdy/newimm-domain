0400f8f7721057c60a6b783327e2680a84fa3ee2
sor4ya <146395929+sor4ya@users.noreply.github.com>
2025-03-04 19:09:12 -0500

    yeeeeee
    yeeeeeeeeee

--- src/main/java/edu/gmu/cs321/Immigrant.java
+++ src/main/java/edu/gmu/cs321/Immigrant.java
@@ -1,9 +1,13 @@
 package edu.gmu.cs321;
 
 public class Immigrant {
-    
+    boolean isValid(int a){
+        return true;
+    }
     boolean true1() {
         return true;
     }
-
+    public boolean update() {
+        return false;
+    }
 }
