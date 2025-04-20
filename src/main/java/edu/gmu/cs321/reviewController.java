d689e1c3742ceaed1c0087db4926b87a7ff39cfb
sor4ya <146395929+sor4ya@users.noreply.github.com>
2025-04-20 18:08:55 -0400

    Fixed the controller of review fxml, and adjusted to naming convention.

--- src/main/java/edu/gmu/cs321/reviewController.java
+++ src/main/java/edu/gmu/cs321/reviewController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;;
 
-public class reviewController {
+public class ReviewController {
     @FXML
     private Label userLabel;
 
